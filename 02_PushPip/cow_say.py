7634e977c4101d443731adf1828f0ef661d94b14
DSFed8889 <FedorovDS@my.msu.ru>
2024-02-27 01:40:46 +0300

    Add -c --cow, -l --list flags

--- 02_PushPip/cow_say.py
+++ 02_PushPip/cow_say.py
@@ -1,12 +1,22 @@
 import argparse
-from cowsay import cowsay
+from cowsay import cowsay, list_cows
 
 
 parser = argparse.ArgumentParser("Cowsay custom program")
 
-parser.add_argument('message', nargs='?',
+parser.add_argument('message',
+                    nargs='?',
                     default='Hello, world!',
                     help='A string to wrap in the text bubble')
+parser.add_argument('-c', '--cow',
+                    default='default',
+                    help='The name of the cow (valid names from list_cows)')
+parser.add_argument('-l', '--list',
+                    action='store_true',
+                    help='List of cows')
 
 args = parser.parse_args()
-print(cowsay(args.message))
+if args.list:
+    print(list_cows())
+else:
+    print(cowsay(args.message, cow=args.cow))
